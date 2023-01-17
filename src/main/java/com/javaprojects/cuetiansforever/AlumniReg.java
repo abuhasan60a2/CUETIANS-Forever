bb409787c185602b0a2d0e91afe7f4d297db9657
abuhasan60a2 <abuhasan1600@gmail.com>
2023-01-17 16:40:26 +0600

    first scene switch successful

--- src/main/java/com/javaprojects/cuetiansforever/AlumniReg.java
+++ src/main/java/com/javaprojects/cuetiansforever/AlumniReg.java
@@ -1,4 +1,24 @@
 package com.javaprojects.cuetiansforever;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AlumniReg {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void backtohome(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("home.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- src/main/java/com/javaprojects/cuetiansforever/Register.java
+++ src/main/java/com/javaprojects/cuetiansforever/Register.java
@@ -1,4 +1,31 @@
 package com.javaprojects.cuetiansforever;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Register {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void studentreg(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Register-2.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void alumnireg(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Register-1.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
